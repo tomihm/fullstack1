c740d7c9f8ef674a0445bf1fc79083d721299c9c
unknown <Cetecom@SJAPCA08LCRD16.duoc.acad>
2026-03-13 13:32:16 -0300

    commit de prueba

--- src/main/java/dev/brushedtom/holamundo/controller/HolaMundoController.java
+++ src/main/java/dev/brushedtom/holamundo/controller/HolaMundoController.java
@@ -1,15 +1,19 @@
 package dev.brushedtom.holamundo.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/saludos")
 public class HolaMundoController {
-
 
     @GetMapping ("/hola")
     public String holaMundo() {
         return "¡HOLA MUNDO!";
+    }
+    @PostMapping("/")
+    public String postHola(){
+        return "Registro Creado";
     }
 }
