8e263719e2e67b95700e7c925b96e2fdc5746edd
tomihm <tomas.hernandez@duocuc.cl>
2026-03-18 18:35:22 -0300

    Tarea terminada con crud y informe

--- src/main/java/dev/brushedtom/holamundo/controller/HolaMundoController.java
+++ src/main/java/dev/brushedtom/holamundo/controller/HolaMundoController.java
@@ -3,6 +3,8 @@ package dev.brushedtom.holamundo.controller;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/saludos")
@@ -15,5 +17,29 @@ public class HolaMundoController {
     @PostMapping("/")
     public String postHola(){
         return "Registro Creado";
+    }
+    Map<Integer, String> productos = new HashMap<>();
+    int id = 1;
+
+    @GetMapping ("/productos")
+    public Map<Integer, String>ver(){
+        return productos;
+    }
+
+    @PostMapping("/productos/{nombre}")
+    public String agregar(@PathVariable String nombre){
+        productos.put(id++, nombre);
+        return "Agregado";
+    }
+    @PutMapping("/productos/{id}/{nombre}")
+    public String camboiar(@PathVariable int id, @PathVariable String nombre){
+        productos.put(id, nombre);
+        return  "Cambiardo";
+    }
+
+    @DeleteMapping("/productos/{id}")
+    public String borrar (@PathVariable int id){
+        productos.remove(id);
+        return "Borrado";
     }
 }
